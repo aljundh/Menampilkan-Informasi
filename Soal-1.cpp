63c1144ae4f335036821da8a2d2ebdd96c5eba79
Hafis Royhan <107126204+aljundh@users.noreply.github.com>
2022-11-16 22:14:05 +0700

    Add files via upload

---
+++ Soal-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	string nama;
+	int berat;
+	double tinggi;
+
+	cout<<"Nama panggilan: ";
+	cin>>nama;
+	cout<<"berat badan: ";
+	cin>>berat;
+	cout<<"Timggi badan (dalam m): "<<endl;
+	cin>>tinggi;
+	
+	cout<<"Informasi Tambahan : "<<endl;
+	cout<<"Nama Panggilan : "<<nama<<endl;
+	cout<<"Berat Badan : "<<berat<<endl;
+	cout<<"Tinggi Badan (dalam m) : "<<tinggi<<endl;
+	
+	
+	return 0;
+}
